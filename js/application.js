f62f8cb36b5de46990895ea843a45c2c5ac5864b
iurevych <iurevych@gmail.com>
2013-03-03 17:55:53 +0200

    Flat-UI v1.0

---
+++ js/application.js
@@ -0,0 +1,51 @@
+// Some general UI pack related JS
+
+$(function () {
+    // Custom selects
+    $("select").dropkick();
+});
+
+$(document).ready(function() {
+    // Todo list
+    $(".todo li").click(function() {
+        $(this).toggleClass("todo-done");
+    });
+
+    // Init tooltips
+    $("[data-toggle=tooltip]").tooltip("show");
+
+    // Init tags input
+    $("#tagsinput").tagsInput();
+
+    // Init jQuery UI slider
+    $("#slider").slider({
+        min: 1,
+        max: 5,
+        value: 2,
+        orientation: "horizontal",
+        range: "min",
+    });
+
+    // JS input/textarea placeholder
+    $("input, textarea").placeholder();
+
+    // Make pagination demo work
+    $(".pagination a").click(function() {
+        if (!$(this).parent().hasClass("previous") && !$(this).parent().hasClass("next")) {
+            $(this).parent().siblings("li").removeClass("active");
+            $(this).parent().addClass("active");
+        }
+    });
+
+    $(".btn-group a").click(function() {
+        $(this).siblings().removeClass("active");
+        $(this).addClass("active");
+    });
+
+    // Disable link click not scroll top
+    $("a[href='#']").click(function() {
+        return false
+    });
+
+});
+
---
+++ js/custom_checkbox_and_radio.js
@@ -0,0 +1,53 @@
+// Custom checkbox and radios
+function setupLabel() {
+    // Checkbox
+    var checkBox = ".checkbox";
+    var checkBoxInput = checkBox + " input[type='checkbox']";
+    var checkBoxChecked = "checked";
+    var checkBoxDisabled = "disabled";
+
+    // Radio
+    var radio = ".radio";
+    var radioInput = radio + " input[type='radio']";
+    var radioOn = "checked";
+    var radioDisabled = "disabled";
+
+    // Checkboxes
+    if ($(checkBoxInput).length) {
+        $(checkBox).each(function(){
+            $(this).removeClass(checkBoxChecked);
+        });
+        $(checkBoxInput + ":checked").each(function(){
+            $(this).parent(checkBox).addClass(checkBoxChecked);
+        });
+        $(checkBoxInput + ":disabled").each(function(){
+            $(this).parent(checkBox).addClass(checkBoxDisabled);
+        });
+    };
+
+    // Radios
+    if ($(radioInput).length) {
+        $(radio).each(function(){
+            $(this).removeClass(radioOn);
+        });
+        $(radioInput + ":checked").each(function(){
+            $(this).parent(radio).addClass(radioOn);
+        });
+        $(radioInput + ":disabled").each(function(){
+            $(this).parent(radio).addClass(radioDisabled);
+        });
+    };
+};
+
+$(document).ready(function(){
+    $("html").addClass("has-js");
+
+    // First let's prepend icons (needed for effects)
+    $(".checkbox, .radio").prepend("<span class='icon'></span><span class='icon-to-fade'></span>");
+
+    $(".checkbox, .radio").click(function(){
+        setupLabel();
+    });
+    setupLabel();
+});
+
