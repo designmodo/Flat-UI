3b921f3581ba9c0c30a49647b7683796b028d379
Dmitry Pytkin <tutbaker@gmail.com>
2014-01-16 13:51:31 +0800

    Update 2.1.2

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Flat UI Free 2.1.1
+Flat UI Free 2.1.2
 =======
 
 Flat UI Free is licensed under a Creative Commons Attribution 3.0 Unported (CC BY 3.0)  (http://creativecommons.org/licenses/by/3.0/) and MIT License - http://opensource.org/licenses/mit-license.html. 
@@ -10,17 +10,16 @@ You are allowed to use these elements anywhere you want, however we’ll highly 
 + [Demo Page](http://designmodo.github.io/Flat-UI/)
 + [Download HTML](https://github.com/designmodo/Flat-UI/archive/master.zip) (for developers)
 + [Download PSD](http://designmodo.com/flat-free/) (for designers)
-+ [Flat UI Pro 1.2.2](http://designmodo.com/flat/) (from $39)
++ [Flat UI Pro 1.2.4](http://designmodo.com/flat/) (from $39)
 
 ## Changelog
 
-
++ 2.1.2 
++ 2.1.1 Typography improved (new global variables, padding and margins changed for basic elements, global default font-size now is 18px, with a line-height of 1.722, added support for ```<abbr>```, ```<address>```, ```<code>```, ```<pre>```, right-aligned ```<blockquote>``), Buttons module completely rewritten (new global variables, buttons now require a .btn-default for the standard version, added support for .btn-link), fixed some bugs in .navbar, mixins updated (added standard mixins from Bootstrap 3), layout updated for stylized select and dropdown menu, various small bugfixes.
++ 2.1 Bootstrap upgraded to 3.0, better box model, update html5 video player to version 4.1, added components documentation and sturt-up template, various small bugfixes.
 + 2.0 Compatibility with Flat UI Pro (Less instead of Sass), Bootstrap upgraded to 2.3.1, grids refinements, various Less compiler bugfixes, Video module bugfix, various small bugfixes.
-+ + 2.1 Bootstrap upgraded to 3.0, better box model, update html5 video player to version 4.1, added components documentation and sturt-up template, various small bugfixes.
-+ + 2.1.1 Typography improved (new global variables, padding and margins changed for basic elements, global default font-size now is 18px, with a line-height of 1.722, added support for <abbr>, <address>, <code>, <pre>, right-aligned <blockquote>), Buttons module completely rewritten (new global variables, buttons now require a .btn-default for the standard version, added support for .btn-link), fixed some bugs in .navbar, mixins updated (added standard mixins from Bootstrap 3), layout updated for stylized select and dropdown menu, various small bugfixes.
-
-+ 1.0 Fixed several small IE bugs
 + 1.1 New icons, Login screen implemented to a real HTML one. Small bug fixes and refinements.
++ 1.0 Fixed several small IE bugs
 
 
 
@@ -41,23 +40,23 @@ Flat UI Free is made using the Lato typeface, which can be downloaded for free h
 
 ##Useful Links (How To)
 
-How to Use UI Kits, Beginners Video Tips:                 [http://designmodo.com/how-to-use/](http://designmodo.com/how-to-use/)
+How to Use UI Kits, Beginners Video Tips:				[http://designmodo.com/how-to-use/](http://designmodo.com/how-to-use/)
 
 
-UI Kit Benefits:											                    [http://designmodo.com/benefits/](http://designmodo.com/benefits/)
+UI Kit Benefits:										[http://designmodo.com/benefits/](http://designmodo.com/benefits/)
 
 
-Create a Website using UI Packs (PSD to HTML) – Day 1: 		[http://designmodo.com/create-website-day-1](http://designmodo.com/create-website-day-1)
+Create a Website using UI Packs (PSD to HTML) – Day 1: 	[http://designmodo.com/create-website-day-1](http://designmodo.com/create-website-day-1)
 
-Create a Website using UI Packs (PSD to HTML) – Day 2: 		[http://designmodo.com/create-website-day-2](http://designmodo.com/create-website-day-2)
+Create a Website using UI Packs (PSD to HTML) – Day 2: 	[http://designmodo.com/create-website-day-2](http://designmodo.com/create-website-day-2)
 
-Create a Website using UI Packs (PSD to HTML) – Day 3: 		[http://designmodo.com/create-website-day-3](http://designmodo.com/create-website-day-3)
+Create a Website using UI Packs (PSD to HTML) – Day 3: 	[http://designmodo.com/create-website-day-3](http://designmodo.com/create-website-day-3)
 
-Create a Website Design using The Bricks UI:				      [http://designmodo.com/website-design-bricks-ui/](http://designmodo.com/website-design-bricks-ui/)
+Create a Website Design using The Bricks UI:			[http://designmodo.com/website-design-bricks-ui/](http://designmodo.com/website-design-bricks-ui/)
 
-What is UI Kit and Why You Should Use it: 					      [http://designmodo.com/ui-kit-use](http://designmodo.com/ui-kit-use)
+What is UI Kit and Why You Should Use it: 				[http://designmodo.com/ui-kit-use](http://designmodo.com/ui-kit-use)
 
-How Companies can Save Time and Money Using UI Kits:		  [http://designmodo.com/save-time-money-using-ui-kits](http://designmodo.com/save-time-money-using-ui-kits)
+How Companies can Save Time and Money Using UI Kits:	[http://designmodo.com/save-time-money-using-ui-kits](http://designmodo.com/save-time-money-using-ui-kits)
 
 UI Kits for Developers – Prototyping and Homemade Design:	[http://designmodo.com/ui-kits-developers](http://designmodo.com/ui-kits-developers)
 
---
+++ js/flatui-fileinput.js
@@ -0,0 +1,215 @@
+/* ===========================================================
+ * flatui-fileinput v0.1.0
+ * Based on Bootstrap: fileinput.js v3.0.0-p7
+ * http://jasny.github.com/bootstrap/javascript.html#fileinput
+ * ===========================================================
+ */
+
++function ($) { "use strict";
+
+  var isIE = window.navigator.appName == 'Microsoft Internet Explorer'
+
+  // FILEUPLOAD PUBLIC CLASS DEFINITION
+  // =================================
+
+  var Fileupload = function (element, options) {
+    this.$element = $(element)
+      
+    this.$input = this.$element.find(':file')
+    if (this.$input.length === 0) return
+
+    this.name = this.$input.attr('name') || options.name
+
+    this.$hidden = this.$element.find('input[type=hidden][name="'+this.name+'"]')
+    if (this.$hidden.length === 0) {
+      this.$hidden = $('<input type="hidden" />')
+      this.$element.prepend(this.$hidden)
+    }
+
+    this.$preview = this.$element.find('.fileinput-preview')
+    var height = this.$preview.css('height')
+    if (this.$preview.css('display') != 'inline' && height != '0px' && height != 'none') this.$preview.css('line-height', height)
+
+    this.original = {
+      exists: this.$element.hasClass('fileinput-exists'),
+      preview: this.$preview.html(),
+      hiddenVal: this.$hidden.val()
+    }
+    
+    this.inline_flag = this.$element.find('.input-group').length;
+    
+    if (this.inline_flag) {
+	    this.$change = this.$element.find('[data-role="change"]');
+	    this.$select_file = this.$element.find('[data-role="select-file"]');
+    }    
+    
+    this.listen()
+  }
+  
+  Fileupload.prototype.listen = function() {
+    this.$input.on('change.bs.fileinput', $.proxy(this.change, this))
+    $(this.$input[0].form).on('reset.bs.fileinput', $.proxy(this.reset, this))
+    
+    if (this.inline_flag) {	    
+	    this.$element.on('mousemove.bs.fileinput', $.proxy(this.checkmove, this))
+	    						 .on('mouseout.bs.fileinput', $.proxy(this.resetControlsClass, this))
+	    						 .on('mousedown.bs.fileinput', $.proxy(this.activateControls, this))
+    }     
+    this.$element.find('[data-trigger="fileinput"]').on('click.bs.fileinput', $.proxy(this.trigger, this));
+    this.$element.find('[data-dismiss="fileinput"]').on('click.bs.fileinput', $.proxy(this.clear, this));       
+  },
+
+  Fileupload.prototype.change = function(e) {
+    if (e.target.files === undefined) e.target.files = e.target && e.target.value ? [ {name: e.target.value.replace(/^.+\\/, '')} ] : []
+    if (e.target.files.length === 0) return
+
+    this.$hidden.val('')
+    this.$hidden.attr('name', '')
+    this.$input.attr('name', this.name)
+
+    var file = e.target.files[0]
+
+    if (this.$preview.length > 0 && (typeof file.type !== "undefined" ? file.type.match('image.*') : file.name.match(/\.(gif|png|jpe?g)$/i)) && typeof FileReader !== "undefined") {
+      var reader = new FileReader()
+      var preview = this.$preview
+      var element = this.$element
+
+      reader.onload = function(re) {
+        var $img = $('<img>') // .attr('src', re.target.result)
+        $img[0].src = re.target.result
+        e.target.files[0].result = re.target.result
+        
+        element.find('.fileinput-filename').text(file.name)
+        
+        // if parent has max-height, using `(max-)height: 100%` on child doesn't take padding and border into account
+        if (preview.css('max-height') != 'none') $img.css('max-height', parseInt(preview.css('max-height'), 10) - parseInt(preview.css('padding-top'), 10) - parseInt(preview.css('padding-bottom'), 10)  - parseInt(preview.css('border-top'), 10) - parseInt(preview.css('border-bottom'), 10))
+        
+        preview.html($img)
+        element.addClass('fileinput-exists').removeClass('fileinput-new')
+
+        element.trigger('change.bs.fileinput', e.target.files)
+      }
+
+      reader.readAsDataURL(file)
+    } else {
+      this.$element.find('.fileinput-filename').text(file.name)
+      this.$preview.text(file.name)
+      
+      this.$element.addClass('fileinput-exists').removeClass('fileinput-new')
+      
+      this.$element.trigger('change.bs.fileinput')
+    }
+  },
+
+  Fileupload.prototype.clear = function(e) {
+    if (e) e.preventDefault()
+    
+    this.$hidden.val('')
+    this.$hidden.attr('name', this.name)
+    this.$input.attr('name', '')
+
+    //ie8+ doesn't support changing the value of input with type=file so clone instead
+    if (isIE) { 
+      var inputClone = this.$input.clone(true);
+      this.$input.after(inputClone);
+      this.$input.remove();
+      this.$input = inputClone;
+    } else {
+      this.$input.val('')
+    }
+
+    this.$preview.html('')
+    this.$element.find('.fileinput-filename').text('')
+    this.$element.addClass('fileinput-new').removeClass('fileinput-exists')
+    
+    if (e !== false) {
+      this.$input.trigger('change')
+      this.$element.trigger('clear.bs.fileinput')
+    }
+  },
+
+  Fileupload.prototype.reset = function() {
+    this.clear(false)
+
+    this.$hidden.val(this.original.hiddenVal)
+    this.$preview.html(this.original.preview)
+    this.$element.find('.fileinput-filename').text('')
+
+    if (this.original.exists) this.$element.addClass('fileinput-exists').removeClass('fileinput-new')
+     else this.$element.addClass('fileinput-new').removeClass('fileinput-exists')
+    
+    this.$element.trigger('reset.bs.fileinput')
+  },
+  
+  Fileupload.prototype.checkmove = function(e) {
+  	var select_file_pos = this.$select_file.offset()["left"],
+  			change_pos = this.$change.offset()["left"];
+	  if (this.$change.is(":hidden")) {
+		  if (e.pageX > select_file_pos && e.pageX < select_file_pos + this.$select_file.width()) {
+			  this.$select_file.addClass("hover");
+		  } else {
+			  this.$select_file.removeClass("hover");
+		  }
+	  } else {
+		  if (e.pageX > change_pos && e.pageX < change_pos + this.$select_file.width()) {
+			  this.$change.addClass("hover");
+		  } else {
+			  this.$change.removeClass("hover");
+		  }
+	  }
+  },
+  
+  Fileupload.prototype.activateControls = function () {
+	  if (this.$change.is(":hidden")) {
+		  this.$select_file.addClass("active");
+	  } else {
+		  this.$change.addClass("active")
+	  }
+  },
+  
+  Fileupload.prototype.resetControlsClass = function () {
+	  this.$change.removeClass("hover active");
+	  this.$select_file.removeClass("hover active");
+  },
+
+  Fileupload.prototype.trigger = function(e) {
+    this.$input.trigger('click')
+    e.preventDefault()
+  }
+
+  
+  // FILEUPLOAD PLUGIN DEFINITION
+  // ===========================
+
+  $.fn.fileinput = function (options) {
+    return this.each(function () {
+      var $this = $(this)
+      , data = $this.data('fileinput')
+      if (!data) $this.data('fileinput', (data = new Fileupload(this, options)))
+      if (typeof options == 'string') data[options]()
+    })
+  }
+
+  $.fn.fileinput.Constructor = Fileupload
+
+
+  // FILEUPLOAD DATA-API
+  // ==================
+
+  $(document).on('click.fileinput.data-api', '[data-provides="fileinput"]', function (e) {      
+    var $target = $(e.target).closest('[data-dismiss="fileinput"], [data-trigger="fileinput"]');
+    if ($target.length > 0) {
+      e.preventDefault()
+      $target.trigger('click.bs.fileinput')
+    }
+  });
+  
+  $(function () {
+    $('[data-provides="fileinput"]').each(function () {
+      var $this = $(this);
+	    if ($this.data('fileinput')) return;
+	    $this.fileinput($this.data());
+    });
+  });
+
+}(window.jQuery);
