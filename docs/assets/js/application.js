0c13009cf6b0818aa18dd5d04d4b7c3f3db8729d
SpyroPress <info@spyropress.com>
2014-11-05 13:06:39 +0500

    Update application js for jQuery UI Slider
    It will also consider the steps while calculating the segments.

--- docs/assets/js/application.js
+++ docs/assets/js/application.js
@@ -7,22 +7,30 @@ String.prototype.repeat = function (num) {
 (function ($) {
 
   // Add segments to a slider
-  $.fn.addSliderSegments = function (amount, orientation) {
-    return this.each(function () {
-      if (orientation === 'vertical') {
-        var output = '';
-        var i;
-        for (i = 1; i <= amount - 2; i++) {
-          output += '<div class="ui-slider-segment" style="top:' + 100 / (amount - 1) * i + '%;"></div>';
-        }
-        $(this).prepend(output);
-      } else {
-        var segmentGap = 100 / (amount - 1) + '%';
-        var segment = '<div class="ui-slider-segment" style="margin-left: ' + segmentGap + ';"></div>';
-        $(this).prepend(segment.repeat(amount - 2));
-      }
-    });
-  };
+  $.fn.addSliderSegments = function () {
+        return this.each(function () {
+            var $this = $(this),
+                option = $this.slider('option'),
+                amount = option.max/option.step,
+                orientation = option.orientation;
+            
+            if ( 'vertical' === orientation ) {
+                
+                var output = '', i;
+                
+                for (i = 1; i <= amount - 2; i++) {
+                    output += '<div class="ui-slider-segment" style="top:' + 100 / (amount - 1) * i + '%;"></div>';
+                }
+                $this.prepend(output);
+                
+            } else {
+                
+                var segmentGap = 100 / (amount) + '%';
+                var segment = '<div class="ui-slider-segment" style="margin-left: ' + segmentGap + ';"></div>';
+                $this.prepend(segment.repeat(amount - 1));
+            }
+        });
+    };
 
   $(function () {
 
